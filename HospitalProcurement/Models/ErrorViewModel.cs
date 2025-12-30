e044cfd0bed1a3dd87969b88d1e0d75315457ef5
Kishore <kishore.ayn@gmail.com>
2025-12-30 08:50:26 +0530

    Add project files.

---
+++ HospitalProcurement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HospitalProcurement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
